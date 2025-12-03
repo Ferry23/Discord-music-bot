be818da78245cddddd6244039dec1f0e18dca3a2
Ferry23 <ferrydepok23@gmail.com>
2025-12-03 12:25:38 +0700

    Add config directory and config.py

---
+++ config/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    DISCORD_TOKEN = os.getenv('')
+    GENIUS_ACCESS_TOKEN = os.getenv('')
+    COMMAND_PREFIX = '!'
+    IDLE_TIMEOUT = 300  # 5 minutes in seconds
+    DEFAULT_VOLUME = 50  # 0-100
+    MAX_QUEUE_SIZE = 100
+    VOTE_SKIP_THRESHOLD = 0.5  # 50%
+
+    @classmethod
+    def validate(cls):
+        if not cls.DISCORD_TOKEN:
+            raise ValueError("DISCORD_TOKEN environment variable is not set")
+        if not cls.GENIUS_ACCESS_TOKEN:
+            raise ValueError("GENIUS_ACCESS_TOKEN environment variable is not set")
